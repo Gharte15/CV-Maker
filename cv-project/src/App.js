b0dfe4492d8cde11d3fbb5a9a9c6fdf951eac5e9
Gharte15 <70962125+Gharte15@users.noreply.github.com>
2023-04-25 15:12:42 +0200

    added navbar

---
+++ cv-project/src/App.js
@@ -0,0 +1,12 @@
+import { Box, Container } from "@mui/system";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
